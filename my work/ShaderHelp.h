285fd1eb9ff9c781bd4a3d8befb7cfbc17006713
Macri-man <admfav2012@gmail.com>
2014-01-17 23:29:28 -0500

    progress

--- my work/ShaderHelp.h
+++ my work/ShaderHelp.h
@@ -1,11 +1,9 @@
 #ifndef SHADERHELP_H_INCLUDED
 #define SHADERHELP_H_INCLUDED
-#include <dirent.h> 
+#include <dirent.h>
+#include "initShaders.h" 
 #include <vector>
 #include <cstring>
-
-vector<GLuint> subShaders;
-map<string,GLuint> shaders;
 
 void printLog(Glenum type, GLuint obj){
 	GLuint logSize;
@@ -34,31 +32,6 @@ void printLog(Glenum type, GLuint obj){
 	delete[] shadeType;
 }
 
-GLuint loadShader(string s,GLenum type){
-string shad;
-
-ifstream in(s.c_str());
-      string line;
-      while ( in.good() ){
-      getline (in,line);
-      shad+=line+"\n";
-        }
-    in.close();
-
-GLchar* vShad = new char [shad.length()+1];
-  strcpy (vShad, shad.c_str());
-
-GLuint vs;
-vs = glCreateShader(type);
-	glShaderSource(vs, 1, (const GLchar**)(&vShad), NULL);
-	glCompileShader(vs);
-	printLog(vs);
-
-delete[] vShad;
-
-return vs;
-}
-
 #ifndef __scanDirectory
 #define __scanDirectory
     void scanDirectory(vector<string> &files, string dirr){
@@ -66,11 +39,10 @@ return vs;
         DIR *dir;
     struct dirent *ent;
     /* open directory stream */
-    dir = opendir (dirr.c_str());
-    if (dir != NULL) {
-
+    dir=opendir(dirr.c_str());
+    if(dir!=NULL){
       /* print all the files and directories within directory */
-      while ((ent = readdir (dir)) != NULL) {
+      while ((ent=readdir (dir))!=NULL){
        // switch (ent->d_type) {
       //  case DT_REG:
           //printf ("%*.*s\n", ent->d_namlen, ent->d_namlen, ent->d_name);
@@ -99,77 +71,31 @@ return vs;
 
 void setupShaders(){
 
-multimap<string,string> shaderPrograms;
-vector<string> files;
-scanDirectory(files,"Shaders");
+	vector<string> files;
+	scanDirectory(files,"Shaders");
+	ShaderInfo shaders[files.size()];
 
-for(int x=0;x<files.size();x++){
-//cout<<files.at(x)<<endl;
+	for(int i=0;i<files.size();i++){
+		int temp=files[i].find(".");
+		string id=files[i].substr(0,temp);
+			switch(id){
+ 	  	 case"vert":shaders[i].type=GL_VERTEX_SHADER;break;
+ 	  	 case"geom":shaders[i].type=GL_GEOMETRY_SHADER_EXT;break;
+ 	  	 case"frag":shaders[i].type=GL_FRAGMENT_SHADER;break;
+ 	  	 case"tess":shaders[i].type=GL_TESS_EVALUATION_SHADER;break;
+ 	  	 case"eval":shaders[i].type=GL_TESS_CONTROL_SHADER;break;
+ 	 	}
+		shaders[i].filename=file.at(i);
+	}
 
-int temp=files.at(x).find(".");
-string id=files.at(x).substr(0,temp);
-
-shaderPrograms.insert(pair<string,string>(id,files.at(x)));
+	initShaders(shaders);
 }
-printf("7:%s\n",gluErrorString(glGetError()));
-GLuint prog=0, shad=0;
-string last="";
-for (multimap<string,string>::iterator it=shaderPrograms.begin(); it!=shaderPrograms.end(); ++it){
-    //std::cout << (*it).first << " => " << (*it).second << '\n';
-    if(it->first!=last){
-    if(last!=""){
-
-        glBindAttribLocation(prog,0,"in_Position");
-        glBindAttribLocation(prog,2,"in_Tex");
-
-    glLinkProgram(prog);
-	printLog(prog);
-	GLint IsLinked;
-        glGetProgramiv(prog, GL_LINK_STATUS, (GLint *)&IsLinked);
-        if(IsLinked==false)
-	cout<<"something isn't linked!!!"<<endl;
-	
-    }
-    prog=glCreateProgram();
-    shaders[it->first]=prog;
-    last=it->first;
-    }
-
-    if(it->second.find("vert")!=string::npos)
-    shad=loadShader("Shaders/"+it->second,GL_VERTEX_SHADER);
-    else if(it->second.find("frag")!=string::npos)
-    shad=loadShader("Shaders/"+it->second,GL_FRAGMENT_SHADER);
-    else if(it->second.find("geom")!=string::npos)
-    shad=loadShader("Shaders/"+it->second,GL_GEOMETRY_SHADER);
-    //
-    //else if(it->second.find("comp")!=string::npos)
-    //shad=loadShader("Shaders/"+it->second,GL_COMPUTE_SHADER);
-    else if(it->second.find("tess")!=string::npos)
-    shad=loadShader("Shaders/"+it->second,GL_TESS_CONTROL_SHADER);
-    else if(it->second.find("eval")!=string::npos)
-    shad=loadShader("Shaders/"+it->second,GL_TESS_EVALUATION_SHADER);
-
-
-    glAttachShader(prog,shad);
-
-}
-printf("8:%s\n",gluErrorString(glGetError()));
-for(map<string,GLuint>::iterator it=shaders.begin();it!=shaders.end();it++)
-cout<<"loaded program "<<it->first<<endl;
-
-
-
-}
-
 
 void setupUniforms(){
-	printf("6:%s\n",gluErrorString(glGetError()));
-    glUseProgram(shaders["main"]);
+
     int i=0;
 
-    printf("5:%s\n",gluErrorString(glGetError()));
     glGetIntegerv(GL_CURRENT_PROGRAM,&i);
-    cout<<shaders["main"]<<" "<<i<<" "<<(int)glIsProgram(shaders["main"])<<endl;
     
     glm::mat4 temp;
     //temp=glm::perspective(gameOptions.fov, (gameOptions.RESOLUTION_X)/((float)gameOptions.RESOLUTION_Y), 2.0f, 5000.0f);
@@ -199,14 +125,6 @@ void setupUniforms(){
 
 
     glUseProgram(0);
-
-}
-
-void cleanupShaders(){
-
-for(int x=0;x<subShaders.size();x++)
-glDeleteShader(subShaders.at(x));
-
 
 }
 
